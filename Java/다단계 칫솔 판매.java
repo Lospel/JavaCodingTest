41ed4a6c0e3a286d24ea78029f7bb05cc0c7440d
KimDowi <rlaehdnl@gmail.com>
2023-11-16 22:52:02 +0900

    2023-11-16

---
+++ Java/다단계 칫솔 판매.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution 
+{
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) 
+    {
+        int[] answer = new int [enroll.length];
+        
+        Map<String, String> parentMap = new HashMap<>();
+        Map<String, Integer> memberIndexMap = new HashMap<>();
+        
+        for(int i=0; i < enroll.length; i++)
+        {
+            parentMap.put(enroll[i], referral[i]);
+            memberIndexMap.put(enroll[i], i);
+        }
+        for(int i=0; i<seller.length; i++)
+        {
+            String nowSeller = seller[i];
+            int profit = 100 * amount[i];
+            
+            while(!nowSeller.equals("-"))
+            {
+                int profitForParent = profit / 10;
+                int nowProfit = profit - profitForParent;
+                
+                answer[memberIndexMap.get(nowSeller)] += nowProfit;
+                
+                nowSeller = parentMap.get(nowSeller);
+                profit /= 10;
+                if(profit < 1)
+                {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
